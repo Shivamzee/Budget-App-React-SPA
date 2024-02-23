8cb0d91b288c1636e03b1d3840083937b5be02a5
Shivamzee <hello.shivamzee@gmail.com>
2024-02-23 15:14:32 +0530

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // import "./App.css";
+
 import { Fragment } from "react";
 import NavBar from "./Components/NavBar";
 function App() {
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,5 @@
 import Container from "react-bootstrap/Container";
 import { Stack, Button } from "react-bootstrap";
-// import Nav from "react-bootstrap/Nav";
-// import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
 
 const NavBar = () => {
   return (
