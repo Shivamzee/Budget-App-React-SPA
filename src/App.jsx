bfc22fe6abc938fa869461528353ebd890801dec
Shivamzee <hello.shivamzee@gmail.com>
2024-02-23 17:14:12 +0530

    ready to create multiple card

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@
 
 import { Fragment } from "react";
 import NavBar from "./Components/NavBar";
+import BudgetCard from "./Components/BudgetCard";
 function App() {
+  const cardStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill , minmax(300px , 1fr))",
+    gap: "1rem",
+    alignItems: "flex-start",
+  };
+
   return (
     <Fragment>
       <NavBar />
+      <div style={cardStyle}>
+        <BudgetCard name="Books" amount={200} max={1500}></BudgetCard>
+      </div>
     </Fragment>
   );
 }
---
+++ src/Components/BudgetCard.jsx
@@ -0,0 +1,21 @@
+import { currencyFormatter } from "../utils";
+import Card from "react-bootstrap/Card";
+const BudgetCard = ({ name, amount, max }) => {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
+          <div className="me-2">{name}</div>
+          <div className="d-flex align-items-baseline">
+            {currencyFormatter.format(amount)}
+            <span className="text-muted fs-6 ms-1">
+              / {currencyFormatter.format(max)}
+            </span>
+          </div>
+        </Card.Title>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default BudgetCard;
